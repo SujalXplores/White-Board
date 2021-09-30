ca2c0b68554ce696694acb004bc4c25d7dc6eb79
SujalShah3234 <shahc9437@gmail.com>
2021-09-30 21:06:29 +0530

    🧐 removed un-necessory imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { SelectionModel } from "@angular/cdk/collections";
 import { Component } from "@angular/core";
-import { MatCheckboxChange } from "@angular/material/checkbox";
-import { MatSelectionListChange } from "@angular/material/list";
 
 @Component({
   selector: 'app-root',
